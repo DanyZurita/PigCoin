4253c7a582d84fc4f89e1cb3373c72ef8966b48c
Dany Zurita <dzurita@cifpfbmoll.eu>
2020-03-01 19:43:33 +0000

    feat(Transaction): Aadir atributos, un contructor vacio y otro que reciba 6 elementos

--- src/main/java/edu/elsmancs/PigCoin/Transaction.java
+++ src/main/java/edu/elsmancs/PigCoin/Transaction.java
@@ -1,6 +1,25 @@
 package edu.elsmancs.PigCoin;
 
+import java.security.PrivateKey;
+import java.security.PublicKey;
+
 
 public class Transaction {
+    private String hash;
+    private String prev_hash;
+    private PublicKey pKey_Sender;
+    private PublicKey pKey_recipient;
+    private double pigcoins = 0d;
+    private String message = null;
     
+    public Transaction () {}
+    
+    public Transaction (String hash, String prev_hash, PublicKey senderPK, PublicKey recipientPK, double pigcoins, String message) {
+        this.hash = hash;
+        this.prev_hash = prev_hash;
+        this.pKey_Sender = senderPK;
+        this.pKey_recipient = recipientPK;
+        this.pigcoins = pigcoins;
+        this.message = message;
+    }
 }
