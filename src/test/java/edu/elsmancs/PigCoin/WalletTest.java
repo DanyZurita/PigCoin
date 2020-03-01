bcfbfe1b1a20d802238310900c13380a8c0016d7
Dany Zurita <dzurita@cifpfbmoll.eu>
2020-03-01 13:48:11 +0000

    test(WaelletTest): Aadir casos test para la PK y SK

--- src/test/java/edu/elsmancs/PigCoin/WalletTest.java
+++ src/test/java/edu/elsmancs/PigCoin/WalletTest.java
@@ -9,4 +9,18 @@ public class WalletTest {
         Wallet wall = new Wallet();
         assertNotEquals(wall.hashCode(), null);
     }
+    
+    @Test
+    public void walletPK() {
+        Wallet wall = new Wallet();
+        wall.generateKeyPair();
+        assertNotEquals(wall.getAddress().hashCode(), null);
+    }
+    
+    @Test
+    public void walletSK() {
+        Wallet wall = new Wallet();
+        wall.generateKeyPair();
+        assertNotEquals(wall.getSKey().hashCode(), null);
+    }
 }
