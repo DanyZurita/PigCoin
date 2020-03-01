fe63003f548bf2eea425a17a002ebf63f5b5ea31
Dany Zurita <dzurita@cifpfbmoll.eu>
2020-03-01 22:59:01 +0000

    test(BlockChainTest): Aadir caso test agregando una Transaction

--- src/test/java/edu/elsmancs/PigCoin/BlockChainTest.java
+++ src/test/java/edu/elsmancs/PigCoin/BlockChainTest.java
@@ -5,4 +5,11 @@ import static org.junit.Assert.*;
 
 public class BlockChainTest {
     
+    @Test
+    public void createBlockChain() {
+        BlockChain block = new BlockChain();
+        Transaction trans = new Transaction();
+        block.addOrigin(trans);
+        assertEquals(block.getBlockChain().get(0), trans);
+    }
 }
