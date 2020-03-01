1406f83621384977c8d4e547efb550292533ed10
Dany Zurita <dzurita@cifpfbmoll.eu>
2020-03-01 21:02:23 +0000

    feat(BlockChain): Inicializar la lista de Transactions y constructor

--- src/main/java/edu/elsmancs/PigCoin/BlockChain.java
+++ src/main/java/edu/elsmancs/PigCoin/BlockChain.java
@@ -1,6 +1,11 @@
 package edu.elsmancs.PigCoin;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class BlockChain {
+    private final List<Transaction> BlockChain = new ArrayList<>();
     
+    public BlockChain() {}
 }
