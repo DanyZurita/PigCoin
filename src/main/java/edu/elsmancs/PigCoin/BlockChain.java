4ea4880e1cd6a4e77fbf03e1469e22f3c6b613d3
Dany Zurita <dzurita@cifpfbmoll.eu>
2020-03-02 15:19:46 +0000

    feat(BlockChain): Aador loads de wallet, input y output

--- src/main/java/edu/elsmancs/PigCoin/BlockChain.java
+++ src/main/java/edu/elsmancs/PigCoin/BlockChain.java
@@ -2,6 +2,7 @@ package edu.elsmancs.PigCoin;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.security.PublicKey;
 
 
 public class BlockChain {
@@ -26,5 +27,32 @@ public class BlockChain {
         if (position <= BlockChain.size() && position >= 0) {
         System.out.print(BlockChain.get(position).toString());
         }
+    }
+    
+    public double loadInputTransaction(PublicKey address) {
+        double input = 0d;
+        for (Transaction trans : BlockChain) {
+            if (trans.getpKey_recipient().equals(address)) {
+                input += trans.getPigcoins();
+            }
+        }
+        return input;
+    }
+
+    public double loadOutputTransaction(PublicKey address) {
+        double output = 0d;
+        for (Transaction trans : BlockChain) {
+            if (trans.getpKey_sender().equals(address)) {
+                output += trans.getPigcoins();  
+            }   
+        }
+        return output;
+    }
+    
+    public double[] loadWallet(PublicKey address) {
+        double input = loadInputTransaction(address);
+        double output = loadOutputTransaction(address);
+        double[] inputOutput = {input, output};
+        return inputOutput;
     }
 }
