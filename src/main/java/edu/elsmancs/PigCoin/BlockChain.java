4fc904481568867990ef313c0620620a20d216b1
Dany Zurita <dzurita@cifpfbmoll.eu>
2020-03-10 17:46:56 +0100

    refactor(BlockChain): Reducir la visibilidad de todos los metodos

--- src/main/java/edu/elsmancs/PigCoin/BlockChain.java
+++ src/main/java/edu/elsmancs/PigCoin/BlockChain.java
@@ -16,52 +16,52 @@ public class BlockChain {
         return BlockChain;
     }
     
-    public void addOrigin(Transaction trans) {
+    void addOrigin(Transaction trans) {
         BlockChain.add(trans);
     }
     
-    public void summarize(){
+    void summarize(){
         BlockChain.forEach((trans) -> {System.out.print(trans.toString());});
     }
 
     
-    public void summarize(int position){
+    void summarize(int position){
         if (position <= BlockChain.size() && position >= 0) {
         System.out.print(BlockChain.get(position).toString());
         }
     }
     
-    public List<Transaction> loadInputTransaction(PublicKey address) {
+    List<Transaction> loadInputTransaction(PublicKey address) {
         List<Transaction> inputTransactions = getBlockChain().stream()
                 .filter((trans) -> (trans.getpKey_recipient().equals(address)))
                 .collect(Collectors.toCollection(ArrayList<Transaction>::new));
         return inputTransactions;
     }
 
-    public List<Transaction> loadOutputTransaction(PublicKey address) {
+    List<Transaction> loadOutputTransaction(PublicKey address) {
         List<Transaction> outputTransactions = getBlockChain().stream()
                 .filter((trans) -> (trans.getpKey_sender().equals(address)))
                 .collect(Collectors.toCollection(ArrayList<Transaction>::new));
         return outputTransactions;
     }
     
-    public List[] loadWallet(PublicKey address) {
+    List[] loadWallet(PublicKey address) {
         List<Transaction> input = loadInputTransaction(address);
         List<Transaction> output = loadOutputTransaction(address);
         List[] inputOutput = {input, output};
         return inputOutput;
     }
     
-    public void processTransactions(PublicKey address, PublicKey pKey_recipient, Map<String, Double> consumedCoins, String message, byte[] signedTransaction) {
+    void processTransactions(PublicKey address, PublicKey pKey_recipient, Map<String, Double> consumedCoins, String message, byte[] signedTransaction) {
         isSignatureValid(address, message, signedTransaction);
         createTransaction(address, pKey_recipient, consumedCoins, message, signedTransaction);
     }
     
-    public boolean isSignatureValid(PublicKey address, String message, byte[] signedTransaction) {
+    private boolean isSignatureValid(PublicKey address, String message, byte[] signedTransaction) {
         return GenSig.verify(address, message, signedTransaction);
     }
     
-    public void createTransaction(PublicKey address, PublicKey pKey_recipient, Map<String, Double> consumedCoins, String message, byte[] signedTransaction) {
+    private void createTransaction(PublicKey address, PublicKey pKey_recipient, Map<String, Double> consumedCoins, String message, byte[] signedTransaction) {
         for (String hash : consumedCoins.keySet()){
             boolean CA = hash.startsWith("CA_");
             if (CA) {
